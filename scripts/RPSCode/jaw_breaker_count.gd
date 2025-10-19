e742f6936deea377a208c860b1da1fcd31ce2394
JAZMINSDESKTOP\prinj <prinjazmin@gmail.com>
2025-10-19 12:58:49 -0500

    Text shows up!

--- scripts/RPSCode/jaw_breaker_count.gd
+++ scripts/RPSCode/jaw_breaker_count.gd
@@ -1,9 +1,15 @@
 extends Label
 
 var doors_node 
+var won_label
+var lost_label
+var tie_label
 
 func _ready():
 	doors_node = get_node("../Doors")
+	won_label = get_node("../You Won!")
+	lost_label = get_node("../You Lost!")
+	tie_label = get_node("../Try Again")
 	
 func _process(delta):
 	self.text = str(JawBreakerCounter.JBCounter)
@@ -18,8 +24,15 @@ func _on_jaw_breaker_pressed() -> void:
 	await PlayRps.play()
 	var result = PlayRps.play()
 	
-	if result == "tie":  
+	if result == "won":
+		won_label.visible = true
+	elif result == "lost":
+		lost_label.visible = true
+	elif result == "tie": 
+		tie_label.visible = true 
 		await get_tree().create_timer(2).timeout 
+		tie_label.visible = false 
 		await doors_node.compChoiceDoorClose()
 		PlayRps.tryAgain() 
 		await doors_node.retry()  
+		return
--- scripts/RPSCode/scissors_count.gd
+++ scripts/RPSCode/scissors_count.gd
@@ -1,9 +1,15 @@
 extends Label
 
 var doors_node 
+var won_label
+var lost_label
+var tie_label
 
 func _ready():
 	doors_node = get_node("../Doors")
+	won_label = get_node("../You Won!")
+	lost_label = get_node("../You Lost!")
+	tie_label = get_node("../Try Again")
 
 func _process(delta):
 	self.text = str(ScissorsCounter.SCounter)
@@ -16,8 +22,15 @@ func _on_scissors_pressed() -> void:
 	doors_node.compChoiceDoor()
 	var result = PlayRps.play()
 	
-	if result == "tie":
+	if result == "won":
+		won_label.visible = true
+	elif result == "lost":
+		lost_label.visible = true
+	elif result == "tie":
+		tie_label.visible = true 
 		await get_tree().create_timer(2).timeout 
+		tie_label.visible = false 
 		await doors_node.compChoiceDoorClose()
 		PlayRps.tryAgain() 
 		await doors_node.retry() 
+		return
