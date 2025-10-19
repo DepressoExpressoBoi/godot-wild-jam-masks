c2fc0a3c84cdd8496aa1ba15c2c9d7225ae4c95c
Tyler Conwell <wgc14@txstate.edu>
2025-10-18 22:09:48 -0500

    demo_frankenstein mask and button

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -10,11 +10,25 @@ func _on_start_button_pressed() -> void:
 	print("Start pressed")
 	get_tree().change_scene_to_file("res://scenes/mask_demo.tscn")
 
-
 func _on_option_button_pressed() -> void:
 	print("options pressed")
-
+	get_tree().change_scene_to_file("res://scenes/option_list.tscn")
 
 func _on_exit_button_pressed() -> void:
 	print("exit pressed")
 	get_tree().quit()
+
+
+
+
+func _on_sound_button_pressed() -> void:
+	print("sound pressed")
+	get_tree().change_scene_to_file("res://scenes/mask_demo.tscn")
+
+func _on_picture_button_pressed() -> void:
+	print("picture pressed")
+	get_tree().change_scene_to_file("res://scenes/option_list.tscn")
+
+func _on_back_button_pressed() -> void:
+	print("back pressed")
+	get_tree().change_scene_to_file("res://scenes/mask_demo.tscn")
